31a1f68cf4d0b2f9cb4bdb5a5de34509c3856734
crAy <craycoding@gmail.com>
2024-07-07 16:43:30 +0900

    add 20 naejeon - 2

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -20,6 +20,11 @@ recording = False
 recording_channel = None
 message_log = []
 
+line_person = [0, 0, 0, 0, 0]
+user_info = [[], [], [], [], []]
+current_view = None
+view_message = None
+
 def sort_participants(participants: set):
     challenger_users = list()
     grandmaster_users = list()
@@ -153,6 +158,259 @@ def sort_participants(participants: set):
 
     return result
 
+def sort_twenty_members(participants: list):
+    challenger_users = list()
+    grandmaster_users = list()
+    master_users = list()
+    diamond_users = list()
+    emerald_users = list()
+    platinum_users = list()
+    gold_users = list()
+    silver_users = list()
+    bronze_users = list()
+    iron_users = list()
+    unranked_users = list()
+
+    for user in participants:
+        splitted_user_profile = user.split('/')
+        user_tier_non_strip = splitted_user_profile[1]
+        user_tier = user_tier_non_strip.strip()
+
+        user_level = user_tier[0].upper()
+
+        if user_level == 'C':
+            user_score = int(user_tier[1:])
+            challenger_users.append((user_score, user))
+
+        if user_level == 'G' and user_tier[1].upper() == 'M':
+            user_score = int(user_tier[2:])
+            grandmaster_users.append((user_score, user))
+
+        if user_level == 'M':
+            user_score = int(user_tier[1:])
+            master_users.append((user_score, user))
+
+        if user_level == 'D':
+            user_score = int(user_tier[1:])
+            diamond_users.append((user_score, user))
+
+        if user_level == 'E':
+            user_score = int(user_tier[1:])
+            emerald_users.append((user_score, user))
+
+        if user_level == 'P':
+            user_score = int(user_tier[1:])
+            platinum_users.append((user_score, user))
+
+        if user_level == 'G' and user_tier[1].upper() != 'M':
+            user_score = int(user_tier[1:])
+            gold_users.append((user_score, user))
+
+        if user_level == 'S':
+            user_score = int(user_tier[1:])
+            silver_users.append((user_score, user))
+
+        if user_level == 'B':
+            user_score = int(user_tier[1:])
+            bronze_users.append((user_score, user))
+
+        if user_level == 'I':
+            user_score = int(user_tier[1:])
+            iron_users.append((user_score, user))
+
+        if user_level == 'U':
+            user_score = 0
+            unranked_users.append((user_score, user))
+
+    user_result = list()
+
+    if challenger_users:
+        challenger_users.sort(key=lambda pair: pair[0], reverse=True)
+        for user in challenger_users:
+            user_result.append(user[1])
+
+    if grandmaster_users:
+        grandmaster_users.sort(key=lambda pair: pair[0], reverse=True)
+        for user in grandmaster_users:
+            user_result.append(user[1])
+
+    if master_users:
+        master_users.sort(key=lambda pair: pair[0], reverse=True)
+        for user in master_users:
+            user_result.append(user[1])
+
+    if diamond_users:
+        diamond_users.sort(key=lambda pair: pair[0])
+        for user in diamond_users:
+            user_result.append(user[1])
+
+    if emerald_users:
+        emerald_users.sort(key=lambda pair: pair[0])
+        for user in emerald_users:
+            user_result.append(user[1])
+
+    if platinum_users:
+        platinum_users.sort(key=lambda pair: pair[0])
+        for user in platinum_users:
+            user_result.append(user[1])
+
+    if gold_users:
+        gold_users.sort(key=lambda pair: pair[0])
+        for user in gold_users:
+            user_result.append(user[1])
+
+    if silver_users:
+        silver_users.sort(key=lambda pair: pair[0])
+        for user in silver_users:
+            user_result.append(user[1])
+
+    if bronze_users:
+        bronze_users.sort(key=lambda pair: pair[0])
+        for user in bronze_users:
+            user_result.append(user[1])
+
+    if unranked_users:
+        for user in unranked_users:
+            user_result.append(user[1])
+
+
+    return user_result
+
+def get_user_tier_score(user: str):
+    splitted_user_profile = user.split('/')
+    user_name = splitted_user_profile[0].strip()
+    user_tier = splitted_user_profile[1].strip()
+
+    user_level = user_tier[0].upper()
+
+    result = 300
+
+    if user_level == 'C':
+        user_score = int(user_tier[1:])
+        user_editted_score = (user_score // 100) * 10
+        result -= user_editted_score
+
+    if user_level == 'G' and user_tier[1].upper() == 'M':
+        user_score = int(user_tier[2:])
+        user_editted_score = (user_score // 100) * 10
+        result -= user_editted_score
+
+    if user_level == 'M':
+        user_score = int(user_tier[1:])
+        user_editted_score = (user_score // 100) * 10
+        result -= user_editted_score
+
+    if user_level == 'D':
+        user_score = int(user_tier[1:])
+        result += (user_score * 20)
+
+    if user_level == 'E':
+        user_score = int(user_tier[1:])
+        result += 80
+        result += (user_score * 20)
+
+    if user_level == 'P':
+        user_score = int(user_tier[1:])
+        result += 160
+        result += (user_score * 20)
+
+    if user_level == 'G' and user_tier[1].upper() != 'M':
+        user_score = int(user_tier[1:])
+        result += 240
+        result += (user_score * 20)
+
+    if user_level == 'S':
+        user_score = int(user_tier[1:])
+        result += 320
+        result += (user_score * 20)
+
+    if user_level == 'B':
+        user_score = int(user_tier[1:])
+        result += 400
+        result += (user_score * 20)
+
+    if user_level == 'I':
+        user_score = int(user_tier[1:])
+        result += 480
+        result += (user_score * 20)
+
+    if user_level == 'U':
+        result = 1000000
+
+    return result
+
+def get_team_head_number():
+    global user_info
+
+    min_diff = 9999999
+    line_number = 0
+
+
+    for i in range(len(user_info)):
+        max_score = 0
+        min_score = 9999999
+        for user_message_info in user_info[i]:
+            user = user_message_info[0]
+            user_score = get_user_tier_score(user)
+
+            if user_score > max_score:
+                max_score = user_score
+            if user_score < min_score:
+                min_score = user_score
+
+        diff = max_score - min_score
+
+        if diff >= 0 and diff < min_diff:
+            min_diff = diff
+            line_number = i
+
+    return line_number
+
+
+def get_team_head_lineup(line_number: int):
+    global user_info
+
+    result = ''
+    result += f'=========================================\n\n'
+
+    user_result = user_info[line_number]
+
+    for i in range(len(user_result)):
+        result += f'{i+1}팀\n'
+        user_score = get_user_tier_score(user_result[i][0])
+        result += f'{user_result[i][0]} : {user_score}\n\n'
+
+    result += f'========================================='
+
+    return result
+
+def get_twenty_user_lineup(head_line_number: int):
+    global user_info
+
+    line = ['탑','정글','미드','원딜','서폿']
+
+    index = 1
+
+    result = ''
+    result += f'=========================================\n\n'
+
+    for line_number in range(len(user_info)):
+        if line_number == head_line_number:
+            continue
+
+        user_result = user_info[line_number]
+
+        result += f'{line[line_number]}\n'
+
+        for i in range(len(user_result)):
+            result += f'{index}. {user_result[i][0]}\n'
+            index += 1
+
+    result += f'========================================='
+
+    return result
+
+
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user}')
@@ -170,9 +428,8 @@ async def start_game(ctx, *, message='모바시'):
 
 @bot.command(name='20인내전')
 async def start_twenty_game(ctx, *, message='모바시'):
+    global line_person, user_info, current_view, view_message
     line = ['탑','정글','미드','원딜','서폿']
-    line_person = [0,0,0,0,0]
-    user_info = [[],[],[],[],[]]
 
     class MyView(View):
         def __init__(self):
@@ -198,8 +455,6 @@ async def start_twenty_game(ctx, *, message='모바시'):
             self.add_item(mid_button)
             self.add_item(ad_button)
             self.add_item(sup_button)
-
-
         def top_callback(self, button):
             line_number = 0
             line_name = line[line_number]
@@ -223,12 +478,15 @@ async def start_twenty_game(ctx, *, message='모바시'):
 
                 if flag:
                     line_person[line_number] += 1
-                    message = await ctx.send(f'{username} 님이 {line_name}으로 참여합니다!')
-                    user_info[line_number].append((username,message.id))
+                    if line_person[line_number] >= 4:
+                        await ctx.send(f'{username} 님이 {line_name}으로 대기합니다. (대기는 아직 개발 중입니다...) ')
+                    else:
+                        message = await ctx.send(f'{username} 님이 {line_name}으로 참여합니다!')
+                        user_info[line_number].append((username,message.id))
 
 
                 button.label = f"{line_name} : {line_person[line_number]}"
-                if(line_person[line_number] == 4):
+                if(line_person[line_number] >= 4):
                     button.style = discord.ButtonStyle.red
                 else:
                     button.style = discord.ButtonStyle.gray
@@ -260,11 +518,14 @@ async def start_twenty_game(ctx, *, message='모바시'):
 
                 if flag:
                     line_person[line_number] += 1
-                    message = await ctx.send(f'{username} 님이 {line_name}로 참여합니다!')
-                    user_info[line_number].append((username, message.id))
+                    if line_person[line_number] >= 4:
+                        await ctx.send(f'{username} 님이 {line_name}으로 대기합니다. (대기는 아직 개발 중입니다...) ')
+                    else:
+                        message = await ctx.send(f'{username} 님이 {line_name}으로 참여합니다!')
+                        user_info[line_number].append((username, message.id))
 
                 button.label = f"{line_name} : {line_person[line_number]}"
-                if (line_person[line_number] == 4):
+                if (line_person[line_number] >= 4):
                     button.style = discord.ButtonStyle.red
                 else:
                     button.style = discord.ButtonStyle.gray
@@ -296,11 +557,14 @@ async def start_twenty_game(ctx, *, message='모바시'):
 
                 if flag:
                     line_person[line_number] += 1
-                    message = await ctx.send(f'{username} 님이 {line_name}로 참여합니다!')
-                    user_info[line_number].append((username, message.id))
+                    if line_person[line_number] >= 4:
+                        await ctx.send(f'{username} 님이 {line_name}으로 대기합니다. (대기는 아직 개발 중입니다...) ')
+                    else:
+                        message = await ctx.send(f'{username} 님이 {line_name}으로 참여합니다!')
+                        user_info[line_number].append((username, message.id))
 
                 button.label = f"{line_name} : {line_person[line_number]}"
-                if (line_person[line_number] == 4):
+                if (line_person[line_number] >= 4):
                     button.style = discord.ButtonStyle.red
                 else:
                     button.style = discord.ButtonStyle.gray
@@ -332,11 +596,14 @@ async def start_twenty_game(ctx, *, message='모바시'):
 
                 if flag:
                     line_person[line_number] += 1
-                    message = await ctx.send(f'{username} 님이 {line_name}로 참여합니다!')
-                    user_info[line_number].append((username, message.id))
+                    if line_person[line_number] >= 4:
+                        await ctx.send(f'{username} 님이 {line_name}으로 대기합니다. (대기는 아직 개발 중입니다...) ')
+                    else:
+                        message = await ctx.send(f'{username} 님이 {line_name}으로 참여합니다!')
+                        user_info[line_number].append((username, message.id))
 
                 button.label = f"{line_name} : {line_person[line_number]}"
-                if (line_person[line_number] == 4):
+                if (line_person[line_number] >= 4):
                     button.style = discord.ButtonStyle.red
                 else:
                     button.style = discord.ButtonStyle.gray
@@ -368,11 +635,14 @@ async def start_twenty_game(ctx, *, message='모바시'):
 
                 if flag:
                     line_person[line_number] += 1
-                    message = await ctx.send(f'{username} 님이 {line_name}으로 참여합니다!')
-                    user_info[line_number].append((username, message.id))
+                    if line_person[line_number] >= 4:
+                        await ctx.send(f'{username} 님이 {line_name}으로 대기합니다. (대기는 아직 개발 중입니다...) ')
+                    else:
+                        message = await ctx.send(f'{username} 님이 {line_name}으로 참여합니다!')
+                        user_info[line_number].append((username, message.id))
 
                 button.label = f"{line_name} : {line_person[line_number]}"
-                if (line_person[line_number] == 4):
+                if (line_person[line_number] >= 4):
                     button.style = discord.ButtonStyle.red
                 else:
                     button.style = discord.ButtonStyle.gray
@@ -381,9 +651,32 @@ async def start_twenty_game(ctx, *, message='모바시'):
 
             return callback
 
-    view = MyView()
+    current_view = MyView()
 
-    await ctx.send(embed = discord.Embed(title='개발자에게 후원이 필요해요'),view=view)
+    view_message = await ctx.send(embed = discord.Embed(title='20인 내전'),view=current_view)
+    await ctx.send(f'@everyone 20인 내전 {message}')
+
+@bot.command(name='20인내전마감')
+async def twenty_end_game(ctx):
+    global user_info, line_person, current_view, view_message
+
+    if current_view:
+        current_view.clear_items()
+        await view_message.edit(view=current_view)
+
+    people = line_person[0] + line_person[1] + line_person[2] + line_person[3] + line_person[4]
+
+    team_head_line_number = get_team_head_number()
+
+    await ctx.send(get_team_head_lineup(team_head_line_number))
+    await ctx.send(get_twenty_user_lineup(team_head_line_number))
+
+    await ctx.send(f'감사합니다 여러분. 총 참여 인원은 {people}명 입니다.')
+    current_view = None
+    view_message = None
+    user_info = [[], [], [], [], []]
+    line_person = [0,0,0,0,0]
+
 
 @bot.command(name='마감')
 async def end_game(ctx):
